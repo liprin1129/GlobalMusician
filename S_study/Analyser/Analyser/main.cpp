e9db7407821094e4f2c94b11008eeef9f23a43f5
liprin1129 <liprin1129@gmail.com>
2020-10-20 21:06:49 +0900

    Create a Xcode project for stream music analyser

---
+++ S_study/Analyser/Analyser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Analyser
+//
+//  Created by SeongMuk Gang on 20/10/2020.
+//  Copyright © 2020 GlobalMusic. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
